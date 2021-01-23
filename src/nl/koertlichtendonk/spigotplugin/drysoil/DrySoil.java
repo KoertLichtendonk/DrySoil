41933494657cc0b4690fb0775c2a21ad129307d3
Koert Lichtendonk <C.Lichtendonk@live.nl>
2021-01-23 23:58:31 +0100

    First commit

---
+++ src/nl/koertlichtendonk/spigotplugin/drysoil/DrySoil.java
@@ -0,0 +1,58 @@
+package nl.koertlichtendonk.spigotplugin.drysoil;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DrySoil extends JavaPlugin implements Listener
+{
+    @Override
+    public void onEnable() {
+
+        getLogger().info("DrySoil has been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+    @EventHandler
+    public void onBlockFade(BlockFadeEvent event)
+    {
+        if (event.getNewState().getBlock().getType().equals(Material.FARMLAND)) {
+            event.setCancelled(true);
+        }
+    }
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        //Define the player
+        Player player = event.getPlayer();
+        //Define the item in their hand
+        Material mat = player.getInventory().getItemInMainHand().getType();
+
+        //If the player right clicks a block
+        if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+
+            //Get clicked block
+            Block clicked_block = event.getClickedBlock();
+            Material clicked_block_mat = clicked_block.getType();
+
+            // Is clicked block soil? Turn to dirt.
+            if(clicked_block_mat == Material.FARMLAND) {
+
+                //Check if they clicked with a shovel
+                if (mat.toString().endsWith("SHOVEL")) {
+                    clicked_block.setType(Material.DIRT);
+                }
+            }
+        }
+    }
+}
